0e9429b0df1a11b1ca567e85b045bfd13c68d9ba
hgarcia-quantum <hgarcia@quantumltda.com>
2025-04-06 11:06:41 -0500

    feat: pagina de productos

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 
-function Card({ src, title, description, id }) {
+function Card({ src, title, description, id, price }) {
+
+  const setDescription = (text) => `${text.substr(0, 155)}...`
   return (
     <>
-      <div key={id} className="card">
-        <img src={src} className="card-img-top" alt={title} />
+      <div key={id} className="card h-100">
+        <img
+          src={src}
+          className="card-img-top img-fluid p-3"
+          alt={title}
+          style={{
+            height: '200px',
+            objectFit: 'contain'
+          }}
+        />
         <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">{description}</p>
-          <a href="#" className="btn">Another link</a>
+          <h5 className="card-title text-muted">{title}</h5>
+          <h5 className="card-subtitle my-2 ">${price}</h5>
+          <p className="card-text">{setDescription(description)}</p>
+          <div className="text-center">
+            <a href="#" className="btn btn-dark">Mas Información</a>
+          </div>
         </div>
       </div>
     </>
--- src/page/Products.jsx
+++ src/page/Products.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import Card from '../components/Card';
 
 function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((res) => res.json())
+      .then((json) => setProducts(json));
+  }, []);
+
   return (
-    <div>Products</div>
-  )
+    <div className="container mt-4">
+      <div className="row">
+        {products.map((product) => (
+          <div
+            key={product.id}
+            className="col-12 col-lg-4 d-flex align-items-stretch mb-4"
+          >
+            <Card
+              key={product.id}
+              id={product.id}
+              price={product.price}
+              title={product.title}
+              src={product.image}
+              description={product.description}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Products
+export default Products;
